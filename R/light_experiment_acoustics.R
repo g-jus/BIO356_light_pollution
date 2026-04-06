b9755512e817f0667a4f987071e73f8ba4a4fb30
g-jus <gusun5199@uib.no>
2026-04-06 15:50:01 +0200

    Adding comments to script for clarity.

--- R/light_experiment_acoustics.R
+++ R/light_experiment_acoustics.R
@@ -1,5 +1,13 @@
+# ============================================================================
+## 1. Setup - loading packages.
+# ============================================================================
+
 library(tidyverse)
 library(here)
+
+# ============================================================================
+## 2. Loading and cleaning data.
+# ============================================================================
 
 pearlside_data <- read.table(here("data/Stox_SurveyEstimatePearlside_2026/output/baseline/LengthDistribution/LengthDistributionData.txt"),
                         header = TRUE,
